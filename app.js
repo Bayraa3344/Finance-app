136f1fb316c17d73025d9e038f2a8b0cca0a8454
Bayraa <bayraal3344@gmail.com>
2020-08-31 15:35:44 +0800

    Event listener holbow enter bolon towchind

--- app.js
+++ app.js
@@ -1,25 +1,7 @@
 // Эдгээрийг function далдлалт гэдэг.
 
 // user interface controller буюу дэлгэцийн controller module
-var uiController = (function () {
-  var x = 150;
-  //   object return хийх ёстой
-  function add(y) {
-    return x + y;
-  }
-  return {
-    add: function (y) {
-      console.log(x + y);
-    },
-    publicAdd: function (a) {
-      console.log("хүлээн авсан утга бол: " + a);
-    },
-    publicAdd2: function (a) {
-      a = add(a);
-      console.log("Боловсруулсан утга: " + a);
-    },
-  };
-})();
+var uiController = (function () {})();
 
 // finance controller Санхүүгийн controller module
 var financeController = (function () {})();
@@ -29,7 +11,20 @@ var appController = (function (
   uiController,
   financeController /* parametr утгаа авч байна */
 ) {
-  uiController.add(50);
-  uiController.publicAdd(30);
-  uiController.publicAdd2(10);
+  var ctrlAddItem = function () {
+    // 1. Оруулсан өгөгдөлүүдээ олж авна.
+    console.log("gggg");
+    // 2. Олж авсан өгөгдөлүүдээ Санхүүгийн controller-т дамжуулж өгнө.
+    // 3. Олж авсан өгөгдлөө дэлгэцэн дээр тохирох хэсэгт гаргана.
+    // 4. Төсвийг тооцоолно.
+    // 5. Эцсийн үлдэгдэл тооцоог дэлгэцэнд гаргана.
+  };
+  document.querySelector(".add__btn").addEventListener("click", function () {
+    ctrlAddItem();
+  });
+  document.addEventListener("keypress", function (event) {
+    if (event.keyCode === 13) {
+      ctrlAddItem();
+    }
+  });
 })(uiController, financeController); /* IIFE argument-руу дамжуулж байна. */
